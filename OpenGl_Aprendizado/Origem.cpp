5134c638550bc3d7eeca9036c99b4b47b9012f44
mateus <mateusulisses2000@gmail.com>
2024-05-15 22:19:59 -0300

    Desenhando o último poligono

--- OpenGl_Aprendizado/Origem.cpp
+++ OpenGl_Aprendizado/Origem.cpp
@@ -3,19 +3,54 @@
 void Desenhar(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 0.0, 0.0);
+
+    // Desenhar linha azul
+    glColor3f(1.0, 0.0, 1.0);
+    glPushMatrix();
+    glTranslatef(-0.5, 0.5, 0.0); // Transladar para a posição desejada
+    glScalef(0.5, 0.5, 0.5);
+    glBegin(GL_LINES);
+    glVertex2f(0.0, 0.0);
+    glVertex2f(1.0, -1.0);
+    glEnd();
+    glPopMatrix();
+
+    // Desenhar quadrado azul
+    glColor3f(0.0, 0.0, 1.0);
+    glPushMatrix();
+    glTranslatef(-0.5, 0.0, 0.0); // Transladar para a posição desejada
+    glScalef(0.5, 0.5, 0.5);
     glBegin(GL_QUADS);
-    glVertex2f(1.0, 1.0);
+    glVertex2f(0.0, 0.0);
+    glVertex2f(0.0, -1.0);
+    glVertex2f(-1.0, -1.0);
+    glVertex2f(-1.0, 0.0);
+    glEnd();
+    glPopMatrix();
+
+    // Desenhar triângulo
+    glColor3f(0.0, 1.0, 0.0);
+    glPushMatrix();
+    glTranslatef(0.5, 0.5, 0.0); // Transladar para a posição desejada
+    glScalef(0.5, 0.5, 0.5);
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0.0, 0.0);
     glVertex2f(1.0, -1.0);
     glVertex2f(-1.0, -1.0);
-    glVertex2f(-1.0, 1.0);
     glEnd();
+    glPopMatrix();
 
-    glColor3f(0.0, 0.0, 1.0);
-    glBegin(GL_LINES);
-    glVertex2f(-1.0, 1.0);
-    glVertex2f(1.0, -1.0);
-    glEnd();
+    //Desenhar Poligono qualquer
+    
+    glColor3f(1.0, 1.0, 1.0);
+    glPushMatrix();
+    glTranslatef(-0.5, -0.5, 0.0);
+    glScalef(0.5, 0.5, 0.5);
+    glBegin(GL_POLYGON);
+    glVertex2f(0.0, 0.0); 
+    glVertex2f(0.0, -0.5); 
+    glVertex2f(1.0, -0.5); 
+    glVertex2f(1.0, 0.0); 
     glFlush();
 }
 
@@ -26,8 +61,8 @@ void Inicializar()
 
 int main(int argc, char** argv)
 {
-    glutInit(&argc, argv); // Initialize GLUT
-    glutInitWindowSize(400, 400);
+    glutInit(&argc, argv);
+    glutInitWindowSize(800, 800);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("Desenho");
 
